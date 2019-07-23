f9ff5c2521f5437fac59cbcbbbdbd4859f18e18d
Jacob Irwin and Tae Lee <jacob@>
2019-07-23 14:51:11 -0700

    This is setup for each page with form action.

--- app.rb
+++ app.rb
@@ -4,9 +4,8 @@ also_reload('lib/**/*.rb')
 require('./lib/sphinx.rb')
 
 get('/') do
-"Home"
-erb(:sphinx)
-
+  "Home"
+  erb(:sphinx)
 end
 
 get('/sphinx') do
@@ -15,22 +14,27 @@ get('/sphinx') do
 end
 
 get ('/sphinx/question1') do
-"question for first riddle here"
+  "question for first riddle here"
+  erb(:question1)
 end
 
 get ('/sphinx/question2') do
-"question for next riddle here"
+  "question for next riddle here"
+  erb(:question2)
 end
 get ('/sphinx/question3') do
-"question for last riddle here"
+  "question for last riddle here"
+  erb(:question3)
 end
 
 get ('/sphinx/success') do
-"user has passed the gates and entered thebes"
+  "user has passed the gates and entered thebes"
+  erb(:success)
 end
 
 get ('/sphinx/fail') do
-"user has failed the test of knowledge"
+  "user has failed the test of knowledge"
+  erb(:fail)
 end
 
 #-----------------------------
