f5ac210895e1d74cae82859ba7260528ad39083c
Jacob Irwin and Tae Lee <jacob@>
2019-07-23 16:06:30 -0700

    create working routes to complete sphinx web page

--- app.rb
+++ app.rb
@@ -30,17 +30,27 @@ get ('/sphinx/question2') do
 end
 get ('/sphinx/question3') do
   "question for last riddle here"
-  erb(:question3)
+  input = params[:question2]
+  @answer = q2(input)
+  if @answer
+    erb(:question3)
+  else
+    erb(:fail)
+  end
 end
-
 get ('/sphinx/success') do
   "user has passed the gates and entered thebes"
-  erb(:success)
+  input = params[:question3]
+  @answer = q3(input)
+  if @answer
+    erb(:success)
+  else
+    erb(:fail)
+  end
 end
-
-get ('/sphinx/fail') do
-  "user has failed the test of knowledge"
-  erb(:fail)
-end
+# get ('/sphinx/fail') do
+#   "user has failed the test of knowledge"
+#   erb(:fail)
+# end
 
 #-----------------------------
